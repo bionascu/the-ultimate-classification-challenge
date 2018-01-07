283717ee8bd3a32e34227a9f034ba1b5f3343e3d
Beatrice Ionascu <beatrice.ionascu@nyu.edu>
2018-01-07 23:16:44 +0100

    category counts for images

--- src/data/category_counts.py
+++ src/data/category_counts.py
@@ -6,28 +6,60 @@ from sklearn.externals import joblib
 
 from utils.paths import data_raw_dir, data_processed_dir
 
+
 encoder_cat_id = joblib.load(path.join(path.join(data_processed_dir, 'encoder_cat_id.pickle')))
 categories = pd.read_pickle(path.join(data_processed_dir, 'categories.pickle'))
-category_columns = ['prod_id', 'cat_id', 'cat_1', 'cat_2', 'cat_3', ]
 
-def categories_from_bson(filename):
+
+def product_distrib_from_bson(filename):
     for product in bson.decode_file_iter(open(filename, 'rb')):
         product_id = product['_id']
         category_id = encoder_cat_id.transform([int(product['category_id'])])[0]
         category_levels = categories.loc[category_id]
-        yield [
-            product_id,
-            category_id,
-            category_levels.category_level1,
-            category_levels.category_level2,
-            category_levels.category_level3
-        ]
+        yield {
+                'prod_id': product_id,
+                'cat_id': category_id,
+                'cat_1': category_levels.cat_1,
+                'cat_2': category_levels.cat_2,
+                'cat_3': category_levels.cat_3
+            }
 
 
-product_categories = pd.DataFrame.from_records(
-    categories_from_bson(path.join(data_raw_dir, 'train_example.bson' if environ.get('TEST_RUN') == 'true' else 'train.bson')),
-    columns=category_columns)
+def image_distrib_from_bson(filename):
+    img_id = 0
+    for product in bson.decode_file_iter(open(filename, 'rb')):
+        product_id = product['_id']
+        category_id = encoder_cat_id.transform([int(product['category_id'])])[0]
+        category_levels = categories.loc[category_id]
+        for product_picture in product['imgs']:
+            yield {
+                'prod_id': product_id,
+                'cat_id': category_id,
+                'cat_1': category_levels.cat_1,
+                'cat_2': category_levels.cat_2,
+                'cat_3': category_levels.cat_3,
+                'img_id': img_id
+            }
+            img_id += 1
 
-for col_name in product_categories.columns.drop('prod_id'):
-    category_counts = product_categories[col_name].value_counts()
-    category_counts.to_pickle(path.join(data_processed_dir, f'{col_name}_counts.pickle'))
+
+# Create product distribution (by category)
+product_distrib = pd.DataFrame.from_records(
+    product_distrib_from_bson(path.join(data_raw_dir, 'train_example.bson' if
+    environ.get('TEST_RUN') == 'true' else 'train.bson')))
+
+for col_name in product_distrib.columns.drop('prod_id'):
+    category_counts = product_distrib[col_name].value_counts()
+    category_counts.to_pickle(path.join(data_processed_dir,
+                                        f'{col_name}_prod_distrib.pickle'))
+
+
+# Create image distribution (by category)
+image_distrib = pd.DataFrame.from_records(
+    image_distrib_from_bson(path.join(data_raw_dir, 'train_example.bson' if
+    environ.get('TEST_RUN') == 'true' else 'train.bson')))
+
+for col_name in image_distrib.columns.drop('prod_id', 'img_id'):
+    category_counts = image_distrib[col_name].value_counts()
+    category_counts.to_pickle(path.join(data_processed_dir,
+                                        f'{col_name}_img_distrib.pickle'))
