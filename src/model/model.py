b38b9cc1e602362f61c7fe86828293afc4824021
Federico Baldassarre <baldassarre.fe@gmail.com>
2018-01-08 15:41:20 +0100

    Smaller model

--- src/model/model.py
+++ src/model/model.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+from keras.engine.topology import InputLayer
 
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
@@ -10,6 +11,7 @@ num_classes_cat_2 = 483
 num_classes_cat_3 = 5263
 num_categories = 5270
 
+"""
 with tf.name_scope('network'):
     model = Sequential()
     model.add(Conv2D(1024,
@@ -23,5 +25,27 @@ with tf.name_scope('network'):
 
     model.add(Flatten())
     model.add(Dense(num_categories, activation=None))
+"""
+
+with tf.name_scope('network'):
+    model = Sequential()
+    model.add(InputLayer(input_shape=input_shape))
+    model.add(Conv2D(64,
+                     kernel_size=(1, 1),
+                     activation='relu'))
+    model.add(Dropout(0.25))
+    model.add(Conv2D(128,
+                     kernel_size=(4, 4),
+                     activation='relu'))
+    model.add(Dropout(0.25))
+    model.add(Conv2D(256,
+                     kernel_size=(3, 3),
+                     activation='relu'))
+    model.add(Dropout(0.25))
+
+    model.add(Flatten())
+    model.add(Dense(num_categories, activation=None))
+
+print(model.summary())
 
 
